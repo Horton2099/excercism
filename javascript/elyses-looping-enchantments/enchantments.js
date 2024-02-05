b05c2a0340f9f2e6fb03be1d82aa581c3849d95a
Austin <kenjigw@gmail.com>
2024-02-05 13:20:24 -0600

    implemented array loops

---
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -0,0 +1,40 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+  let total = 0;
+  stack.forEach((value) => {if (value === card) {
+    total++;
+  }});
+  return total;
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  let total = 0;
+  for (const value of stack) {
+    if (type === true) {
+      if (value % 2 === 0) {
+        total++;
+      } 
+    } else if (type === false) {
+      if (value % 2 === 1) {
+        total++;
+      }
+    }
+  }
+  return total;
+}
