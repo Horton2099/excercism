4c2cd901f0dde153bf4ce2a2da64ac88fafd0cc5
Austin <kenjigw@gmail.com>
2024-04-18 19:31:29 -0500

    finished task 2

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -1,5 +1,5 @@
 class Badge {
     public String print(Integer id, String name, String department) {
-        return "["+id+"]"+" - "+name+" - "+department.toUpperCase();
+        return (id != null) ? "["+id+"]"+" - "+name+" - "+department.toUpperCase() : name+" - "+department.toUpperCase();
     }
 }
