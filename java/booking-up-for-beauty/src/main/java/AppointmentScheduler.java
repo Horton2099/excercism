0b9c23bbd756211201649cbad6dc13b40dddf09c
Austin <kenjigw@gmail.com>
2024-05-22 10:03:00 -0500

    finished task 1

---
+++ java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class AppointmentScheduler {
+    public LocalDateTime schedule(String appointmentDateDescription) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy H:mm:ss");
+        return LocalDateTime.parse(appointmentDateDescription, formatter);
+    }
+
+    public boolean hasPassed(LocalDateTime appointmentDate) {
+        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+    }
+
+    public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
+        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+    }
+
+    public String getDescription(LocalDateTime appointmentDate) {
+        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getDescription() method");
+    }
+
+    public LocalDate getAnniversaryDate() {
+        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+    }
+}
