00bb23543f13f4788dfca56061d31d04b1e11012
Austin <kenjigw@gmail.com>
2024-06-23 14:13:33 -0500

    finished needforspeed

---
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -0,0 +1,50 @@
+class NeedForSpeed {
+    private int speed;
+    private int batteryDrained;
+    private int distance;
+    private int totalBattery = 100;
+
+    NeedForSpeed(int speed, int batteryDrain) {
+        this.speed = speed;
+        this.batteryDrained = batteryDrain;
+    }
+
+    public boolean batteryDrained() {
+        if(totalBattery == 0) {
+            return true;
+        };
+        return false;
+    }
+
+    public int distanceDriven() {
+        return distance;
+    }
+
+    public void drive() {
+        if(totalBattery > 0){
+            distance += speed;
+            totalBattery -= batteryDrained;
+        }
+    }
+
+    public static NeedForSpeed nitro() {
+        return new NeedForSpeed(50, 4);
+    }
+}
+
+class RaceTrack {
+    private int distance;
+    RaceTrack(int distance) {
+        this.distance = distance;
+    }
+
+    public boolean tryFinishTrack(NeedForSpeed car) {
+        while(car.distanceDriven() < distance) {
+            if(car.batteryDrained()){
+                return false;
+            }
+            car.drive();
+        }
+        return true;
+    }
+}
