72aa2b4007701208a396f67bac76e907987b14df
Austin <kenjigw@gmail.com>
2024-07-10 16:34:33 -0500

    all tests pass

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,21 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        ;
+        if (operation == null) {
+            throw new IllegalArgumentException("Operation cannot be null");
+        } else if (operation.isEmpty()) {
+            throw new IllegalArgumentException("Operation cannot be empty");
+        } else if (!operation.equals("+") && !operation.equals("*") && !operation.equals("/")) {
+            throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+        } else if (operation.equals("/") && operand2 == 0) {
+            throw new IllegalOperationException("Division by zero is not allowed", new ArithmeticException())
+                    ;
+        }
+        int result = switch (operation) {
+            case "+" -> operand1 + operand2;
+            case "*" -> operand1 * operand2;
+            case "/" -> operand1 / operand2;
+            default -> 0;
+        };
+        return operand1 + " " + operation + " " + operand2 + " = " + result;
     }
 }
